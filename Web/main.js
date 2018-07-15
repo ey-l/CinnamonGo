6890db184617469635585cbff38693c7d3961c30
yujing lai <yujinglai@216-71-221-209.dyn.novuscom.net>
2018-07-14 17:52:01 -0700

    bluetooth

---
+++ Web/main.js
@@ -0,0 +1,53 @@
+
+function printNameOfDevice() {
+
+	navigator.bluetooth.requestDevice({
+  	filters: [{
+    	name: 'Alta HR'
+  	}],
+  	optionalServices: ['battery_service']
+	})
+	.then(device => { 
+		// Human-readable name of the device.
+  		console.log(device.name);
+
+  		// Attempts to connect to remote GATT Server.
+  		return device.gatt.connect();
+	})
+	.catch(error => { console.log(error); });
+
+
+}
+
+function getBatteryLevel() {
+
+	
+	
+	navigator.bluetooth.requestDevice({
+  	filters: [{
+    	name: 'Alta HR'
+  	}],
+  	optionalServices: ['battery_service']
+	})
+	.then(device => device.gatt.connect())
+	.then(server => {
+
+  		// Getting Battery Service...
+  		return server.getPrimaryService('battery_service');
+	})
+	.then(service => {
+
+  		// Getting Battery Level Characteristic...
+  		return service.getCharacteristic('battery_level');
+	})
+	.then(characteristic => {
+  		// Reading Battery Level...
+  		return characteristic.readValue();
+	})
+	.then(value => {
+  		console.log('Battery percentage is ' + value.getUint8(0));
+	})
+	.catch(error => { console.log(error); });
+	
+
+}
